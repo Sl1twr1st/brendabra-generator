c1e8ea96761d3170c423e42500ed4b9713078094
jali <rizalialma@gmail.com>
2023-02-20 09:19:07 +0700

    second revision

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
 
 const Home = () => {
   const maxRetries = 20;
-  const [input, setInput] = useState('');
-  const [img, setImg] = useState(''); 
+  const [input, setInput] = useState("");
+  const [img, setImg] = useState("");
   const [retry, setRetry] = useState(0);
   const [retryCount, setRetryCount] = useState(maxRetries);
   const [isGenerating, setIsGenerating] = useState(false);
-  const [finalPrompt, setFinalPrompt] = useState('');
+  const [finalPrompt, setFinalPrompt] = useState("");
 
   const onChange = (event) => {
     setInput(event.target.value);
   };
 
   const generateAction = async () => {
-    console.log('Generating...');
+    console.log("Generating...");
 
     if (isGenerating && retry === 0) return;
 
@@ -34,22 +34,22 @@ const Home = () => {
 
       setRetry(0);
     }
-    
-    const response = await fetch('/api/generate', {
-      method: 'POST',
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
       headers: {
-        'Content-Type': 'image/jpeg',
+        "Content-Type": "image/jpeg",
       },
       body: JSON.stringify({ input }),
     });
-  
+
     const data = await response.json();
 
     if (response.status === 503) {
-      console.log('Model is loading still :(.')
+      console.log("Model is loading still :(.");
       return;
     }
-  
+
     // If another error, drop error
     if (!response.ok) {
       console.log(`Error: ${data.error}`);
@@ -57,10 +57,9 @@ const Home = () => {
     }
 
     setFinalPrompt(input);
-    setInput('');
+    setInput("");
     setImg(data.image);
     setIsGenerating(false);
-  
   };
 
   const sleep = (ms) => {
@@ -72,10 +71,12 @@ const Home = () => {
   useEffect(() => {
     const runRetry = async () => {
       if (retryCount === 0) {
-        console.log(`Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`);
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`
+        );
         setRetryCount(maxRetries);
         return;
-        }
+      }
 
       console.log(`Trying again in ${retry} seconds.`);
 
@@ -93,73 +94,61 @@ const Home = () => {
 
   return (
     <div className="root">
-  <Head>
-    <title>brenda bra</title>
-  </Head>
-  <div className="container">
-    <div className="header">
-      <div className="header-title">
-
-        <h1>Brenda Bra Generator</h1>
-      </div>
-      <div className="header-subtitle">
-        <h2>
-          Make sure you have "bra" on your prompt. Be creative!
-        </h2>
-        <h2>
-          If you have black result. The internet censor you!
-        </h2>
-        <h2>
-          Keep Prompting until an image appear! Bra for all!
-        </h2>
-        <h2>
-          If button keep active with no result for 30 seconds
-        </h2>
-        <h2>
-          refresh the page.
-        </h2>
-      </div>
-      <div className="prompt-container">
-        <input className="prompt-box" value={input} onChange={onChange} />
-        <div className="prompt-buttons">
-          <a
-            className={
-              isGenerating ? 'generate-button loading' : 'generate-button'
-            }
-            onClick={generateAction}
-          >
-            <div className="generate">
-              {isGenerating ? (
-                <span className="loader"></span>
-              ) : (
-                <p>Generate_Bra</p>
-              )}
+      <Head>
+        <title>brenda bra</title>
+      </Head>
+      <div className="container">
+        <div className="header">
+          <div className="header-title">
+            <h1>Brenda Bra Generator</h1>
+          </div>
+          <div className="header-subtitle">
+            <h2>Make sure you have "bra" on your prompt. Be creative!</h2>
+            <h2>If you have black result. The internet censor you!</h2>
+            <h2>Keep Prompting until an image appear! Bra for all!</h2>
+            <h2>If button keep active with no result for 30 seconds</h2>
+            <h2>refresh the page.</h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={generateAction}
+              >
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate_Bra</p>
+                  )}
+                </div>
+              </a>
             </div>
-          </a>
+          </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={finalPrompt} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
+      </div>
+      <div className="badge-container grow">
+        <a
+          href="https://brendabra.wordpress.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="badge">
+            <p>back to Brenda site</p>
+          </div>
+        </a>
       </div>
     </div>
-    {/* Add output container */}
-    {img && (
-      <div className="output-content">
-        <Image src={img} width={512} height={512} alt={finalPrompt} />
-        <p>{finalPrompt}</p>
-      </div>
-    )}
-  </div>
-  <div className="badge-container grow">
-    <a
-      href="https://brendabra.wordpress.com/"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <div className="badge">
-        
-        <p>brenda.dangerous website</p>
-      </div>
-    </a>
-  </div>
-</div>
   );
 };
 
